26d4e72d9599dd1c28ec3c8a16d3f2aba114fe93
Uko <uko@unikernel.net>
2012-09-23 00:47:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Task Manager
+============
+
+User should be able to:
+-  register in system
+-  login into system
+-  add new task
+-  edit task
+-  delete task
+-  share task (share using email of another user in the system who will be also able to manage task afterwards )
+-  see list of tasks ( list should auto-update when someone shares tasks with user; in the list user should see who has shared task)
+
+_Use Twitter Bootstrap for UI, please (or something analogical). Cover code by Rspec tests, preferably_
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmdChalange::Application.config.secret_token = 'bd168dc16c499acc8e8034205be5247f16bccba0699f4bcc631a274e6cd62d1f39ad2963d582adab2423e0e70f4672e7aadd72a345108c70d527a432216019e5'
